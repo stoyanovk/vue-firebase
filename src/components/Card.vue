2f85f918134e12f663931f29104ee5439c795829
Konstantin <stoyanov.k1992@gmail.com>
2020-09-04 09:43:56 +0300

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" :src="img" :alt="title" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ title }}</div>
+      <p class="text-gray-700 text-base">
+        {{ description }}
+      </p>
+    </div>
+    <!-- <div class="px-6 pt-4 pb-2"> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    img: { type: String, default: "" },
+    title: { type: String, default: "" },
+    description: { type: String, default: "" },
+  },
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>Home</div>
+  <div>
+    <div>Home</div>
+    <Card :title="title" :description="description" :img="img" />
+    <Loader />
+  </div>
 </template>
 
 <script>
+import Card from "@/components/Card";
+import Loader from "@/components/Loader";
 export default {
   name: "Home",
+  data() {
+    return {
+      title: "lol",
+      description: "lol kek chebureck",
+      img: "https://picsum.photos/id/237/200/300",
+    };
+  },
+  components: { Loader, Card },
 };
 </script>
