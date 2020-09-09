4d0469be6961ba325e954f76bcee5a11ded7258c
Konstantin <stoyanov.k1992@gmail.com>
2020-09-09 14:10:36 +0300

    add register

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -45,6 +45,6 @@
 
 <script>
 export default {
-  name: "Sign In",
+  name: "SignIn",
 };
 </script>
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 export default {
   state: {
     user: {},
@@ -5,8 +6,17 @@ export default {
   getters: {},
   mutations: {},
   actions: {
-    register({ commit }, { email, name, password }) {
-      
+    async register({ commit }, { email, name, password }) {
+      console.log(commit);
+      await firebase.auth().createUserWithEmailAndPassword(email, password);
+      const user = await firebase.auth().currentUser;
+      if (user) {
+        await firebase.database().ref(`/user/${user.uid}/info`).set({
+          uid: user.uid,
+          name,
+          email,
+        });
+      }
     },
   },
 };
